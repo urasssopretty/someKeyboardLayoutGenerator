44b816f2961e6debbf939518cd71d1dd3d1dcbbb
you should know that <hsprimate8@gmail.com>
2023-02-26 12:26:19 +0300

    synh

--- generators/layoutGenerator.py
+++ generators/layoutGenerator.py
@@ -4,14 +4,44 @@ from layoutTest.textTest import *
 import string
 
 
-def getKeyPostion(charCounters, char, keyboardType):
+def getKeyPosition(charCounters, character, keyboardType):
+    if keyboardType != "standard":
+        raise Exception("now layout generator dont work with non standard layouts!!!!",
+                        "| error: math based generator in getKeyPosition")
+
+    # ### rewrite for add one loop
+    # for index in range(100):
+    #     for pairIndex in range(len(charCounters)):
+    #         if charCounters[pairIndex][0] == character:
+    #             match pairIndex:
+    #                 case 0:
+    #                     return (2, 0.5)
+    #                 case _:
+    #                     return (-999, -999)
+
+            # if 14 < key["id"] < 28 \
+            #         or 28 < key["id"] < 40 \
+            #         or 41 < key["id"] < 52:
+
+    for pairIndex in range(len(charCounters)):
+        if charCounters[pairIndex][0] == character:
+            match pairIndex:
+                case 0:
+                    return (2, 0.5)
+                case _:
+                    return (-999, -999)
 
 
-    return 0
+### TODO REWRITE IT beacuse key id must be in order from first to last but not like this
+def getKeyId(charCounters, char, keyboardType):
+    if keyboardType != "standard":
+        raise Exception("non-standard types of keyb is unsupported now | error: math based generator in getKeyId")
 
+    for pairIndex in range(len(charCounters)):
+        if charCounters[pairIndex][0] == char:
+            return pairIndex
 
-def getKeyId():
-    return 0
+    raise Exception("character in not declared!!!! | error: math based generator in getKeyID", char, "\n", charCounters)
 
 
 def getFingerIndex(index):
@@ -34,23 +64,29 @@ def mathLayoutGenerator(textFileName, keyboardType, abc=string.ascii_lowercase):
                             ".": ">",
                             "/": "?"
                         }
-    characters = abc + str(shiftSpecialChars.keys())
+
+    characters = abc + ''.join(str(element) for element in list(shiftSpecialChars.keys()))
+    # charCounters = charStats(characters, text)
     charCounters = sorted(charStats(characters, text).items(), key=lambda x: x[1], reverse=True)
 
-    if keyboardType == "standard":
-        keys = [0 for _ in range(len(characters))]
-        key = {}
+    if keyboardType != "standard":
+        raise Exception("HEY NOW ANOTHER KEYBOARD TYPE UNSUPPORTED | ERROR IN MATH BASED KEYB GENERATOR")
 
-        for char in characters:
-            key["primary"] = char
-            key["shift"] = shiftSpecialChars[char] if ( char in shiftSpecialChars.keys() ) else char.upper()
-            key["position"] = getKeyPostion(charCounters, char, keyboardType)
-            key["finger"] = getFingerIndex()
-            key["id"] = getKeyId()
+    keys = []
 
-            keys.append(key)
-    else:
-        print("HEY NOW ANOTHER KEYBOARD TYPE UNSUPPORTED | ERROR IN MATH BASED KEYB GENERATOR")
+    for char in characters:
+        keys.append(
+            {
+                "primary": char,
+                "shift": shiftSpecialChars[char] if (char in shiftSpecialChars.keys()) else char.upper(),
+                "id": getKeyId(charCounters, char, keyboardType),
+                "position": getKeyPosition(charCounters, char, keyboardType),
+                "finger": getFingerIndex(0)
+            }
+        )
+
+    # for key in keys:
+    #     print(key)
 
     # return KeyboardLayout()
 
